f8fc1832c3122aa650888bf8ede719bbd8699260
pwvbutler <54254361+pwvbutler@users.noreply.github.com>
2024-10-18 12:02:25 +0200

    Update simple_gui.py

--- src/ecms_np_analysis/simple_gui.py
+++ src/ecms_np_analysis/simple_gui.py
@@ -6,7 +6,8 @@ from faradaic_efficiency import FaradaicEfficiencyECMS
 import zipfile
 import io
 import tempfile
-
+import datetime
+st.write(datetime.datetime.now().astimezone().tzname())
 
 ##############################################
 ################# FUNCTIONS ##################
@@ -233,12 +234,13 @@ with col1:
             ec_ca = get_ec_data(ec_ca_datafile) if ec_ca_datafile is not None else None
 
             if ec_cp is not None:
+                if datetime.datetime.now().astimezone().tzname() == "UTC":
+                    ec_cp["time/s"]._data -= 7200
                 ecms_cp = ec_cp + ms
                 ecms_cp["raw_potential"]._data = ecms_cp["<Ewe/V>"]._data # quick fix: for some reason raw_potential is not correct
                 ecms_cp.tstamp += ecms_cp.t[0] - 1
 
                 ecms_cp_fig, ecms_cp_axes = plot_ecms_data(ecms_cp, name + " CP")
-                # st.write(ecms_cp_fig)
 
                 ecms_cp_data = calc_faradaic_efficiencies(ecms_cp)
 
